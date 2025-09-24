375be4a480533a7deb0bcb9cd6dc65305481250e
JoaoJunior85 <juniorpaulinopaulino1@gmail.com>
2025-09-24 17:29:33 +0200

    Meu commit temporário

--- EleicoesJava/src/Connection/ConnectionFactory.java
+++ EleicoesJava/src/Connection/ConnectionFactory.java
@@ -14,7 +14,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/sge";
     private static final String USER = "root";
-    private static final String PASS = "manu123@$";
+    private static final String PASS = "";
 
     public static Connection getConnection() {
 
@@ -65,7 +65,7 @@ public class ConnectionFactory {
             }
 
         } catch (SQLException ex) {
-            System.out.println("Coneccao não feichado");
+            System.out.println("Coneccao não encontrada ");
         }
     }
 
